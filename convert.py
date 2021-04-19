a89e964d4ac726cc92863fd74331df106faf4535
IshaanChawla0001 <37042014+IshaanChawla0001@users.noreply.github.com>
2021-04-19 00:41:52 -0400

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = "https://github.com/IshaanChawla0001/DataMining-final-project/blob/main/final_bank_dataset.csv"
+url = "https://raw.githubusercontent.com/IshaanChawla0001/DataMining-final-project/main/final_bank_dataset.csv"
 
 data = pd.read_csv(url,sep=';')
 
